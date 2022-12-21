e4a67e0d7e418d86102c19bc8f456a8d9d3109db
Pedro <spyxtreme41@gmail.com>
2022-12-21 00:30:00 -0300

    Adicionar códigos de erro

---
+++ erros.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef __ERROS_h__
+#define __ERROS_h__
+
+// Constantes
+#define SUCESSO				(0)
+#define ERRO_LENDO_OPCAO	(2022000)
+#define ERRO_CEL_FAH		(2022001)
+#define ERRO_IMC			(2022002)
+
+#endif
